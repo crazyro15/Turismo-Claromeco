cc22b7f8e62aeacce5cf4a6981cb4f7cd31e5879
crazyro15 <crazyro15@hotmail.com>
2022-07-10 18:13:07 -0300

    CRUD listo

--- js/form.js
+++ js/form.js
@@ -1,40 +1,40 @@
 window.onload = function() {
 	
-    var nombre = document.getElementById('nombre');
+    var nombre = document.getElementById('txtNombre');
     nombre.addEventListener("input", campoNombre);
 	
-    var correo = document.getElementById('correo');
+    var correo = document.getElementById('txtPrecio');
     correo.addEventListener("input", campoCorreo);
 
-    var mensaje = document.getElementById('mensaje');
+    var mensaje = document.getElementById('txtStock');
     mensaje.addEventListener("input", campoMensaje);
 	
 }
 
 function campoNombre() {
-    var usuarioNombre = document.getElementById('nombre').value;
+    var usuarioNombre = document.getElementById('txtNombre').value;
 
     if (usuarioNombre) {
         document.getElementById('nombre_error').innerHTML = " ";
-        document.getElementById('nombre').style.outline = "1px solid #000";
+        document.getElementById('txtNombre').style.outline = "1px solid #000";
     }
 }
 
 function campoCorreo() {
-    var usuarioCorreo = document.getElementById('correo').value;
+    var usuarioCorreo = document.getElementById('txtPrecio').value;
 	
     if (usuarioCorreo) {
         document.getElementById('correo_error').innerHTML = " ";
-        document.getElementById('correo').style.outline = "1px solid #000";
+        document.getElementById('txtPrecio').style.outline = "1px solid #000";
     }
 }
 
 function campoMensaje() {
-    var usuarioMensaje = document.getElementById('mensaje').value;
+    var usuarioMensaje = document.getElementById('txtStock').value;
 	
     if (usuarioMensaje) {
         document.getElementById('mensaje_error').innerHTML = " ";
-        document.getElementById('mensaje').style.outline = "1px solid #000";
+        document.getElementById('txtStock').style.outline = "1px solid #000";
     }
 }
 
@@ -45,15 +45,15 @@ function borrando() {
 function validacion() {
     var expUsuario = /[a-zA-Z]{2,30}[0-9]{0}/;
     var expCorreo = /^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/;
-    var usuarioNombre = document.getElementById('nombre').value;
-    var usuarioCorreo = document.getElementById('correo').value;
-    var usuarioMensaje = document.getElementById('mensaje').value;
+    var usuarioNombre = document.getElementById('txtNombre').value;
+    var usuarioCorreo = document.getElementById('txtPrecio').value;
+    var usuarioMensaje = document.getElementById('txtStock').value;
     var nombreError = document.getElementById('nombre_error');
     var correoError = document.getElementById('correo_error');
     var mensajeError = document.getElementById('mensaje_error');
-    var campoNombre = document.getElementById('nombre');
-    var campoCorreo = document.getElementById('correo');
-    var campoMensaje = document.getElementById('mensaje');
+    var campoNombre = document.getElementById('txtNombre');
+    var campoCorreo = document.getElementById('txtPrecio');
+    var campoMensaje = document.getElementById('txtStock');
 
     if (!usuarioNombre) {
     	var mensajeErrorNombre = "Su nombre es obligatorio!";
@@ -90,5 +90,27 @@ function validacion() {
     	campoMensaje.style.outline = "2px solid #f00";
     	return false;
     } 
+    guardar();
     console.log("Enviado")
+}
+
+function guardar() {
+ 
+    let n = document.getElementById("txtNombre").value
+    let p = document.getElementById("txtPrecio").value
+    let s = document.getElementById("txtStock").value
+ 
+    let producto = {
+        nombre: n,
+        mail: p,
+        mensaje: s
+    }
+    let url = "http://localhost:5000/mensajes"
+    var options = {
+        body: JSON.stringify(producto),
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+       // redirect: 'follow'
+    }
+    
 }
